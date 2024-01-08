aa08cbb34127f17c6ae86d3d18b6d3102a6b3799
Dregon42 <lbrantl13@gmail.com>
2024-01-07 23:24:03 -0500

    second commit

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,12 +1,16 @@
-import React, { useState } from 'react'
-import { SafeAreaView, StyleSheet } from 'react-native'
-import { Colors } from '../styles/colors'
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { Colors } from '../styles/colors';
 import { PanGestureHandler } from 'react-native-gesture-handler';
 import { Coordinate, Direction, GestureEventType } from '../types/types';
+import Snake from './Snake';
+import { checkGameOver } from '../utils/checkGameOver';
+import Food from './Food';
+import { checkEatsFood } from '../utils/checkEatsFood';
 
 const SNAKE_INITIAL_POSITION = [{ x: 5, y: 5}];
 const FOOD_INITIAL_POSITION = { x: 5, y: 20};
-const GAME_BOUNDS = { xMin: 0, xMax: 35, yMin: 0, yMax: 63}; //
+const GAME_BOUNDS = { xMin: 0, xMax: 35, yMin: 0, yMax: 1000}; 
 const MOVE_INTERVALS = 50;
 const SCORE_INCREMENT = 10; 
 
@@ -14,8 +18,55 @@ export default function Game(): JSX.Element {
     const [direction, setDirection] = useState<Direction>(Direction.Right);
     const [snake, setSnake] = useState<Coordinate[]>(SNAKE_INITIAL_POSITION); // used "[]" because of SNAKE_INITIAL _POSITION x,y coordinates
     const [food, setFood] = useState<Coordinate>(FOOD_INITIAL_POSITION);
-    const [isGameOver, setISGameOver] = useState<boolean>(false);
-    const [isPaused, setIsPaused] =useState<boolean>(false)
+    const [isGameOver, setIsGameOver] = useState<boolean>(false);
+    const [isPaused, setIsPaused] =useState<boolean>(false);
+    const [score, setScore] =useState<number>(0);
+    
+    useEffect(() => {
+        if (!isGameOver) {
+            const intervalId = setInterval(() => {
+                !isPaused && moveSnake();
+            }, MOVE_INTERVALS)
+
+            return () => clearInterval(intervalId);
+        }
+    }, [snake, isGameOver, isPaused]) // want to run useEffect when the snake changes, gameover, or game pause
+
+    // this function is for moving the snake every 15 milliseconds
+    const moveSnake = () => {
+        const snakeHead = snake[0];
+        const newSnakeHead = { ...snakeHead} // creating a copy
+
+        // game over
+        if (checkGameOver(snakeHead, GAME_BOUNDS)) {
+            setIsGameOver((prev) => !prev);
+            return;
+        }
+
+        switch (direction) {
+            case Direction.Up:
+                newSnakeHead.y -= 1;
+                break;
+            case Direction.Down:
+                newSnakeHead.y += 1;
+                break;
+            case Direction.Left:
+                newSnakeHead.x -= 1;
+                break;
+            case Direction.Right:
+                newSnakeHead.x += 1;
+        }
+
+        // grow snake if it eats food
+        if (checkEatsFood(newSnakeHead, food, 2)) {
+            setSnake([newSnakeHead, ...snake]); // add additional snake body after eating
+
+            // get another position for food
+            setScore(score + SCORE_INCREMENT); //add to score
+        }
+
+        setSnake([newSnakeHead, ...snake.slice(0,-1)]) //remove last element to keep length
+    }
 
     const handleGesture = (event:GestureEventType) => {
         // console.log(event.nativeEvent) // Log of X,Y axis data
@@ -44,7 +95,10 @@ export default function Game(): JSX.Element {
   return (
     <PanGestureHandler onGestureEvent={handleGesture}>
         <SafeAreaView style={styles.container}>
-    
+            <View style={styles.boundaries}>
+                <Snake snake={snake}/>
+                <Food x={food.x} y={food.y} />
+            </View>
         </SafeAreaView>
     </PanGestureHandler>
     )
@@ -55,5 +109,14 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: Colors.primary,
     },
+    boundaries: {
+        flex: 1,
+        marginTop: 20,
+        borderColor: Colors.primary,
+        borderWidth: 12,
+        borderBottomLeftRadius: 20,
+        borderBottomRightRadius: 20,
+        backgroundColor: Colors.background,
+    }
 })
 
--- src/components/Snake.tsx
+++ src/components/Snake.tsx
@@ -1,23 +1,36 @@
 import React, { Fragment } from 'react';
 import { Coordinate } from '../types/types';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Colors } from '../styles/colors';
 
 interface SnakeProps {
     snake: Coordinate[];
 }
 
-export default function Snake({ snake }): JSX.Element {
+export default function Snake({snake}:SnakeProps): JSX.Element {
   return (
     <Fragment>
         {snake.map((segment:Coordinate, index:number) => {
             const segmentStyle = {
-                
+                // size of segment the snake will be moved
+                left: segment.x * 10,
+                top: segment.y *10
             }
 
             return (
-                <View key={index}/>
+                <View key={index} style={[styles.snake, segmentStyle]}/>
             )
         })}
     </Fragment>
   )
 }
+
+const styles = StyleSheet.create({
+    snake: {
+        width: 15,
+        height: 15,
+        borderRadius: 7,
+        backgroundColor: Colors.primary,
+        position: 'absolute',
+    }
+})
